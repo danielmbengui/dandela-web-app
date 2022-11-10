ad078d2bafa978a18ee56f7c1edb8bff15abc65e
Daniel Mbengui <couzmec@MacBook-Air-de-Daniel.local>
2022-11-10 01:58:43 +0100

    with first test

--- components/Login/ErrorLogin.js
+++ components/Login/ErrorLogin.js
@@ -13,69 +13,9 @@ export default function ErrorLogin({logo, firestore}) {
   const [open, setOpen] = React.useState(true);
 
   return (
-    <Grid container
-    direction={'column'}
-    justifyContent={'center'}
-    alignItems={'center'}
-    style={{ width: "100%" }}
->
-    <Grid item p={5} style={{ display: 'block' }}>
-
-        <Image
-            src={logo}
-            //fill
-            //width="responsive"
-            //layout="responsive"
-            width={250}
-            height={250}
-            //height={320}
-            alt="logo"
-            //loading='lazy'
-            priority
-            sizes="(min-width: 60em) 24vw,
-                    (min-width: 28em) 45vw,
-                    100vw"
-            style={{
-                width: "100%",
-                height: "auto"
-            }} />
-    </Grid>
-    <Grid item p={3} style={{ textAlign: 'center' }}>
-    <Box sx={{ width: '100%' }}>
-      <Collapse in={open}>
-        <Alert
-        severity='error'
-        variant='filled'
-          action={
-            <IconButton
-              aria-label="close"
-              color="inherit"
-              size="small"
-              onClick={() => {
-                setOpen(false);
-              }}
-            >
-              <CloseIcon fontSize="inherit" />
-            </IconButton>
-          }
-          sx={{ mb: 2, textAlign: 'start' }}
-        >
-            <AlertTitle sx={{ mb: 0.5, alignItems: 'flex-start' }}>This action is prohibited</AlertTitle>
-        You can't access to this website sorry — <strong>contact the owner!</strong>
-          
-        </Alert>
-      </Collapse>
-      <Link href="/index">
-      <Button
-        color='primary'
-        variant="outlined"
-      >
-        Go to Login
-      </Button>
-      </Link>
-    </Box>
-    </Grid>
-</Grid>
+    <>
+    Error Login
+    </>
 
   );
 }
