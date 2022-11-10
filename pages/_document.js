b4ae50b737ba2db6aea60a60a02b85debf2dbe93
Daniel Mbengui <couzmec@MacBook-Air-de-Daniel.local>
2022-11-11 00:51:46 +0100

    add darkmode for login and manage folder

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
             <Head>
             <meta name="description" content="Created by Daniel Slaver Mbengui" />
             <link rel="icon" href="/favicon.ico" />
-            <script src="https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js"></script>
+            
 <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.css" />
 
             <meta name="application-name" content="PWA App" />
