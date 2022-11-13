544ab1d235cbfabad421a26ce285ac973feceeee
Daniel Mbengui <couzmec@MacBook-Air-de-Daniel.local>
2022-11-13 05:33:34 +0100

    first commit to webapp.dandela.com

--- config.admin.firebase.js
+++ config.admin.firebase.js
@@ -10,6 +10,7 @@ const adminFirebaseConfig = {
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-e28au%40dandelawebapp.iam.gserviceaccount.com"
-  };
+  
+};
 
   export default adminFirebaseConfig;
