3bfaba443e52ccee7aa9b17eab345e86c477766c
Daniel Mbengui <couzmec@MacBook-Air-de-Daniel.local>
2022-11-16 01:07:32 +0100

    remove env file

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,10 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Head from 'next/head'
-import Image from "next/image";
-import Home from '../components/Home/Home';
-import Index from '../components/Index/Index';
-import { firestore, storage } from "../config.firebase";
-import firebase from "../config.firebase";
+import React from 'react';
 import ContainerLogin from '../components/Authentication/ContainerLogin';
 import Login from '../components/Authentication/Login/Login';
 import { withAuthUser, AuthAction } from 'next-firebase-auth'
 import PermanentBackdrop from '../components/Loading/PermanentBackdrop';
 
 const MyLoader = () => <PermanentBackdrop />
-
 const LoginPage = () => <ContainerLogin><Login /></ContainerLogin>
 
 export default withAuthUser({
