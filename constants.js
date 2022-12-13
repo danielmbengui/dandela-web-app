d26c27b93decbc0a2e25bf279d75b0e820f345bb
Daniel Mbengui <couzmec@macbook-air-de-daniel.home>
2022-12-14 00:46:59 +0100

    modify background color app mobile

--- constants.js
+++ constants.js
@@ -2,3 +2,4 @@ export const COMPANY_NAME = 'Dandela';
 export const APP_NAME = 'Dandela Web App';
 export const DEFAULT_SCREEN_MODE = 'light';
 export const COLLECTION_USER = 'USER';
+export const COLLECTION_TRANSFERT = 'TRANSFERT';
