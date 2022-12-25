69f9362fbc61c42479c8c70f4424196c4ac5843d
Daniel Mbengui <couzmec@macbook-air-de-daniel.home>
2022-12-25 02:05:27 +0100

    update new transfert component

--- components/Dashboard/Transfert/NewTransfert.js
+++ components/Dashboard/Transfert/NewTransfert.js
@@ -1,23 +1,88 @@
+import React, { useEffect, useState } from "react";
 import { Button, Stack } from "@mui/material";
-import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import Transfert, { transfertConverter } from "../../../classes/TransfertClass";
 import { COLLECTION_TRANSFERT, USER_TYPE_ADMIN } from "../../../constants";
 import { useUserContext } from "../../../context/UserProvider";
-import { createRandomCode } from "../../../functions/firestore/TransfertFunctions";
+import { createRandomCode, formatTransfertCode } from "../../../functions/firestore/TransfertFunctions";
 import SlideInDialog from "../../MyComponents/SlideInDialog";
+import SnackBarCustom from "../../MyComponents/SnackBarCustom";
 import { TextFieldCustom } from "../../MyComponents/TextFieldCustom";
+import { SnackbarProvider, useSnackbar } from 'notistack';
+
+function SnackBarCustomBis(props) {
+  const { t } = useTranslation('transferts/new');
+  const { enqueueSnackbar } = useSnackbar();
+  const { transfert, showSnackBarSuccess, setShowSnackBarSuccess } = props;
+
+  useEffect(() => {
+    if (showSnackBarSuccess) {
+      enqueueSnackbar(t('messageSucces'), { variant: 'success' });
+      enqueueSnackbar(`${t('Code')} : ${formatTransfertCode(transfert.code)}`);
+      setShowSnackBarSuccess(false);
+    }
+  }, [showSnackBarSuccess])
+
+  return (
+    <>
+    </>
+  );
+}
+
+const ShowSnackBarSuccess = (props) => {
+  const { transfert, showSnackBarSuccess, setShowSnackBarSuccess } = props;
+
+  return (
+    <SnackbarProvider maxSnack={3} autoHideDuration={3000} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+      <SnackBarCustomBis transfert={transfert} showSnackBarSuccess={showSnackBarSuccess} setShowSnackBarSuccess={setShowSnackBarSuccess} />
+    </SnackbarProvider>
+  );
+}
 
 
 
-
-export default function NewTransfert({firestore, logo}) {
-  const { t, i18n } = useTranslation('transferts/new');
+export default function NewTransfert({ langage, firestore, logo }) {
+  const { t } = useTranslation('transferts/new');
 
   const [user, setUser] = useUserContext();
   const [receiver, setReceiver] = useState('');
+  const [errorReceiver, setErrorReceiver] = useState(false);
+  const [messogeReceiver, setMessageReceiver] = useState('');
+  const isErrorReceiver = (_receiver) => {
+    if (!_receiver.toString().length || _receiver.toString().length < 5 || _receiver.toString().length > 30) {
+      setErrorReceiver(true);
+      setMessageReceiver(t('messageErrorReceiver'));
+      return (true);
+    }
+    setErrorReceiver(false);
+    setMessageReceiver("");
+    return (false);
+  }
   const [amount, setAmount] = useState('');
+  const [errorAmount, setErrorAmount] = useState(false);
+  const [messageAmount, setMessageAmount] = useState('');
+  const isErrorAmount = (_amount) => {
+    if (!_amount.toString().length || parseInt(_amount) < 50) {
+      setErrorAmount(true);
+      setMessageAmount(t('messageErrorAmount'));
+      return (true);
+    }
+    setErrorAmount(false);
+    setMessageAmount("");
+    return (false);
+  }
   const [isTransfertValide, setIsTransfertValide] = useState(false);
+  const [transfert, setTransfert] = useState(new Transfert({}));
+  const [showSnackBarSuccess, setShowSnackBarSuccess] = useState();
+
+  function initComponents() {
+    setReceiver('');
+    setErrorReceiver(false);
+    setMessageReceiver('');
+    setAmount('');
+    setErrorAmount(false);
+    setMessageAmount('');
+  }
   /*
   uid: transfert.uid,
             code: transfert.code,
@@ -28,121 +93,102 @@ export default function NewTransfert({firestore, logo}) {
             receipt_sender: transfert.receipt_sender,
             valide: transfert.valide,
   */
- useEffect(() => {
+  useEffect(() => {
     if (user && user.type === USER_TYPE_ADMIN) {
       setIsTransfertValide(true);
     }
- }, [user])
+  }, [user]);
 
- const onChangeReceiver = (event) => {
+  useEffect(() => {
+    if (errorReceiver)
+      setMessageReceiver(t('messageErrorReceiver'));
+    if (errorAmount)
+      setMessageAmount(t('messageErrorAmount'));
+  }, [langage])
+
+  const onChangeReceiver = (event) => {
     setReceiver(event.target.value);
     console.log("RECEIVER", event.target.value);
- }
+  }
 
- const onChangeAmount = (event) => {
-  setAmount(event.target.value);
-  console.log("AMOUNT", event.target.value);
-}
- const addTransfert = () => {
-// Add a new document with a generated id.
-var newTransfertRef = firestore.collection(COLLECTION_TRANSFERT).doc();
-const transfert = new Transfert({});
-const uid = newTransfertRef.id;
-transfert.uid = newTransfertRef.id;
-transfert.code = createRandomCode();
-transfert.receiver = receiver;
-transfert.amount = amount;
-transfert.receipt_receiver = false;
-transfert.receipt_dandela = false;
-transfert.receipt_sender = false;
-transfert.valide = isTransfertValide;
-// later...
-newTransfertRef.withConverter(transfertConverter).set(transfert);
- }
+  const onChangeAmount = (event) => {
+    setAmount(event.target.value);
+    console.log("AMOUNT", event.target.value);
+  }
+  const addTransfert = () => {
+    const _isErrorReceiver = isErrorReceiver(receiver);
+    const _isErrorAmount = isErrorAmount(amount);
+    if (!_isErrorReceiver && !_isErrorAmount) {
+      // Add a new document with a generated id.
+      var newTransfertRef = firestore.collection(COLLECTION_TRANSFERT).doc();
+      const _transfert = new Transfert({});
+      _transfert.uid = newTransfertRef.id;
+      _transfert.code = createRandomCode();
+      _transfert.receiver = receiver;
+      _transfert.amount = amount;
+      _transfert.receipt_receiver = false;
+      _transfert.receipt_dandela = false;
+      _transfert.receipt_sender = false;
+      _transfert.valide = isTransfertValide;
+      // later...
+      newTransfertRef.withConverter(transfertConverter).set(_transfert);
+      initComponents();
+      setTransfert(_transfert);
+      setShowSnackBarSuccess(true);
+    }
+  }
 
- useEffect(() => {
-  console.log("RANDOM code", createRandomCode());
- })
+  useEffect(() => {
+    console.log("RANDOM code", createRandomCode());
+  })
 
   return (
     <>
-    <Stack 
-      direction={'column'}
-      justifyContent={'center'} alignItems={'stretch'}
-      spacing={2}
-      mb={3}
-    >
-      <TextFieldCustom
-        //fullWidth
-        //error={errorName}
-        id="dest"
-        label={t('Receiver')}
-        required
-        //controlled
-        //defaultValue="Hello World"
-        value={receiver}
-        onChange={onChangeReceiver}
-        //helperText={errorName ? "Incorrect entry." : ''}
-        //theme={theme}
-        placeholder={t('Receiver')}
-      />
-      <TextFieldCustom
-        //fullWidth
-        //error={errorName}
-        id="amount"
-        label={t('Amount')}
-        required
-        type={'number'}
-        //controlled
-        //defaultValue="Hello World"
-        value={amount}
-        onChange={onChangeAmount}
-        //helperText={errorName ? "Incorrect entry." : ''}
-        //theme={theme}
-        placeholder={t('Amount')}
-      />
-    </Stack>
+      <Stack
+        direction={'column'}
+        justifyContent={'center'} alignItems={'stretch'}
+        spacing={2}
+        mb={3}
+      >
+        <TextFieldCustom
+          //fullWidth
+          error={errorReceiver}
+          id="receiver"
+          label={t('Receiver')}
+          required
+          //controlled
+          type={'text'}
+          //defaultValue="Hello World"
+          value={receiver}
+          onChange={onChangeReceiver}
+          helperText={messogeReceiver}
+          //theme={theme}
+          placeholder={t('Receiver')}
+        />
+        <TextFieldCustom
+          //fullWidth
+          error={errorAmount}
+          id="amount"
+          label={t('Amount')}
+          required
+          type={'number'}
+          //controlled
+          //defaultValue="Hello World"
+          value={amount}
+          onChange={onChangeAmount}
+          helperText={messageAmount}
+          //theme={theme}
+          placeholder={t('Amount')}
+        />
+      </Stack>
 
-    <Button
-    variant="contained"
-    onClick={addTransfert}
-    >
-      {t('Add')}
-    </Button>
-
-    <Button onClick={() => {
-      let promise = Notification.requestPermission();
-      function notifyMe() {
-        if (!("Notification" in window)) {
-          // Check if the browser supports notifications
-          alert("This browser does not support desktop notification");
-        } else if (Notification.permission === "granted") {
-          // Check whether notification permissions have already been granted;
-          // if so, create a notification
-          const notification = new Notification("Hi there!");
-          console.log("PERMISSION notif GRANTED", 'granted')
-          // …
-        } else if (Notification.permission === "denied") {
-          console.log("PERMISSION notif", 'denied')
-          // We need to ask the user for permission
-          Notification.requestPermission().then((permission) => {
-            // If the user accepts, let's create a notification
-            if (permission === "granted") {
-              const notification = new Notification("Hi there!");
-              console.log("PERMISSION notif DENIED", 'granted')
-              // …
-            }
-          });
-        }
-      
-        // At last, if the user has denied notifications, and you
-        // want to be respectful there is no need to bother them anymore.
-      }
-      notifyMe();
-    }}>
-      Notif
-    </Button>
-    
+      <Button
+        variant="contained"
+        onClick={addTransfert}
+      >
+        {t('Add')}
+      </Button>
+      <ShowSnackBarSuccess transfert={transfert} showSnackBarSuccess={showSnackBarSuccess} setShowSnackBarSuccess={setShowSnackBarSuccess} />
     </>
   );
 };
