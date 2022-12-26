987a1d1f20181099ef2971e916ca504e5f2a184a
Daniel Mbengui <couzmec@macbook-air-de-daniel.home>
2022-12-26 15:33:14 +0100

    try firebase messaging with adding script to _document

--- pages/firebase-messaging-sw.js
+++ pages/firebase-messaging-sw.js
@@ -12,7 +12,7 @@
 // are not available in the service worker.
 //importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 //importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
-
+import firebase, { app } from "../config.firebase";
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
@@ -31,7 +31,7 @@ const firebaseConfig = {
 
 
 
-const app = firebase.initializeApp(firebaseConfig);
+//const app = firebase.initializeApp(firebaseConfig);
 
 // Initialize Firebase Cloud Messaging and get a reference to the service
 //const messaging = firebase.messaging();
