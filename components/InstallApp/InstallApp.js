f0d4f2317d68895ef6dd8b346850c4f0e2cf4bdb
Daniel Mbengui <couzmec@macbook-air-de-daniel.home>
2022-12-29 10:05:34 +0100

    remove PWAInstallerPrompt

--- components/InstallApp/InstallApp.js
+++ components/InstallApp/InstallApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PWAInstallerPrompt from 'react-pwa-installer-prompt';
+//import PWAInstallerPrompt from 'react-pwa-installer-prompt';
 import { Button, Grid } from '@mui/material';
 import styles from './InstallApp.module.css';
 import { styled } from '@mui/material/styles';
@@ -10,31 +10,37 @@ const MaterialUIButton = styled(Button)(() => ({
   color: 'var(text-secondary)',
 }));
 
-export default function InstallApp(){
+export default function InstallApp() {
 
   return (
-    <PWAInstallerPrompt
-      render={({ onClick }) => (
-        <Grid container direction={'column'}>
-          <Grid item>
-          <div className={styles.grid}>
-            <div className={styles.card}>
-              <p style={{paddingBottom: '2vh',}}>Installer l&apos;application sur mobile/ordinateur.</p>
-              <MaterialUIButton  variant="contained" onClick={onClick}>
-              Install
-            </MaterialUIButton>
-            </div>
-            </div>
+    <>
+      {
+        /*
+  <PWAInstallerPrompt
+        render={({ onClick }) => (
+          <Grid container direction={'column'}>
+            <Grid item>
+            <div className={styles.grid}>
+              <div className={styles.card}>
+                <p style={{paddingBottom: '2vh',}}>Installer l&apos;application sur mobile/ordinateur.</p>
+                <MaterialUIButton  variant="contained" onClick={onClick}>
+                Install
+              </MaterialUIButton>
+              </div>
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
-      )}
-      callback={(data) => (
-        <>
-        {console.log('RESULT InstallApp', data)}
-        <span>MEEEEEEEEERDE</span>   
-        </>
-        
-      )}
-    />
+        )}
+        callback={(data) => (
+          <>
+          {console.log('RESULT InstallApp', data)}
+          <span>MEEEEEEEEERDE</span>   
+          </>
+          
+        )}
+      />
+        */
+      }
+    </>
   )
 }
