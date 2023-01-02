d435799ec85ae56dbf24ec988acbd454623c1f16
Daniel Mbengui <couzmec@macbook-air-de-daniel.home>
2023-01-02 22:22:28 +0100

    manage notification on good way

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -78,10 +78,10 @@ messaging.onMessage((payload) => {
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = 'Background Message Title';
   const notificationOptions = {
     body: 'Background Message body.',
-    icon: payload.notification.icon,
+    icon: '/img/logo.png',
   };
   self.registration.showNotification(notificationTitle,
     notificationOptions);
